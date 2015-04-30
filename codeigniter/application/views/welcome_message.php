468d0e468a4b0a485ef6992ecd82845b862efff1
Jesús <speed_chucho@hotmail.com>
2015-04-29 19:38:49 -0500

    Modificaciones a la vista de bienvenida

--- codeigniter/application/views/welcome_message.php
+++ codeigniter/application/views/welcome_message.php
@@ -3,75 +3,38 @@
 <head>
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
-
-	<style type="text/css">
-
-	::selection{ background-color: #E13300; color: white; }
-	::moz-selection{ background-color: #E13300; color: white; }
-	::webkit-selection{ background-color: #E13300; color: white; }
-
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
-
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body{
-		margin: 0 15px 0 15px;
-	}
-	
-	p.footer{
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-	
-	#container{
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		-webkit-box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+        <link type="text/css" href="/bootstrap/css/bootstrap.min.css">
 </head>
 <body>
-    <form method="POST" action="">
-        <label for="txtUsuario">Nombre usuario:</label>
-        <input type="text" id="txtUsuario" name="txtUsuario"/>
-        <label for="txtPassword">Contraseña:</label>
-        <input type="text" id="txtPassword" name="txtPassword"/>
+    <p class="bg-primary">INICIAR SESIÓN</p>
+    <p class="bg-info">BIENVENIDO A ADMINISTRA HORARIOS UV</p>
+    <form>
+        <div class="form-group">
+            <label for="txtUsuario">Nombre usuario:</label>
+            <input type="text" id="txtUsuario" name="txtUsuario"/>
+        </div>
+        <div class="form-group">
+            <label for="txtPassword">Contraseña:</label>
+            <input type="text" id="txtPassword" name="txtPassword"/>
+        </div>
+        <button type="button" class="btn btn-primary">Iniciar sesión</button>
         <input type="submit" id="btnSubmit" value="Iniciar sesion"/>
+        <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
     </form>
+    
+    <div class="dropdown">
+  <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-expanded="true">
+    Carrera
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
+    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Informática</a></li>
+    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Estadística</a></li>
+    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">TIC</a></li>
+    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Ingeniería</a></li>
+    <li role="presentation"><a role="menuitem" tabindex="-1" href="#">Redes</a></li>
+  </ul>
+</div>
         
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
