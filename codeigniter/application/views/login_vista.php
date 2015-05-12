816255c0f246e46a097691bfea2fe30b0130711a
Jesús <speed_chucho@hotmail.com>
2015-05-12 18:14:14 -0500

    Inicio sesión fácil

--- codeigniter/application/views/login_vista.php
+++ codeigniter/application/views/login_vista.php
@@ -13,7 +13,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css" type="text/css"/>
 </head>
 <body>
      
