aaafd9f28459f7f9465d892ee93c00cd80af0805
Jesús <speed_chucho@hotmail.com>
2015-05-12 18:02:55 -0500

    realizando cambios

--- codeigniter/application/views/login_vista.php
+++ codeigniter/application/views/login_vista.php
@@ -6,4 +6,39 @@
  * and open the template in the editor.
  */
 
-<h1>Prueba</h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bienvenido sistema horarios</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="bootstrap-3.3.4-dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+     
+<form action="principal.php" method="post" class="login">
+    <title> INICIO</title>
+    <h1>Iniciar sesión</h1>
+    <footer>&copy; Sistema</footer>
+    <div><label>Usuario:</label><input name="user" type="text" ></div>
+    <div><label>Contraseña:</label><input name="password" type="password"></div>
+    <div><input name="login" type="submit" value="Ingresar"></div>
+    <div><a href=registrar.php>Registrarse</a></div>
+    <a href=Invitado.php>Invitado</a>
+</form>
+<form>
+    <div class="form-group">
+        <label for="NombreUsuario">Usuario:</label>
+        <input type="Usuario" class="form-control" id="UsuarioEntrada" placeholder="Ingresar usuario">
+    </div>    
+
+    <h6>
+        <p>Sistema creado en la UV por estudiantes de la Licenciatura en Informática:</p>
+	<p>@Sirio</p>
+	<p>@Beatriz</p>
+	<p>@Julián</p>
+	<p>@Jesús</p>
+    </h6>
+</body>
+</html>
