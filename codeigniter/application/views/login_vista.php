c733e65611e9387172cbd43783c7083a19c9592b
Jesús <speed_chucho@hotmail.com>
2015-05-19 06:42:39 -0500

    Vistas e idioma

--- codeigniter/application/views/login_vista.php
+++ codeigniter/application/views/login_vista.php
@@ -1,32 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>INICIAR SESIÓN</title>
-        <!--
-        <link rel="stylesheet" href="../bootstrap/css/bootstrap-theme.css.map" type="text/css"/>
-        -->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-</head>
-<body> 
+<?php
+    $this->load->view('head/head_vista');
+    $this->load->view('header/header_vista');
+?>
     <h1>Iniciar sesión</h1>
-    <form method="post" class="login">
-<!--    <form action="<?php echo base_url('index.php/Welcome/login');?>" method="post" class="login">    
-
-        <div><label>Usuario:</label>
-            <input name="user" type="text" >
-        </div>
-        <div><label>Contraseña:</label>
-            <input name="password" type="password">
-        </div>
-        <div>
-            <input name="login" type="submit" value="Ingresar">
-        </div>
-        <div><a href=registrar.php>Registrarse</a></div>
-        <a href=Invitado.php>Invitado</a>
-    </form>
-    <form>
---> 
+<!--    -->
+    <form action="<?php echo base_url('index.php/Welcome/login');?>" method="post" class="login">     
     <div class="container-fluid">
         <div class="form-group">
             <label for="NombreUsuario">Usuario:</label>
@@ -43,13 +21,21 @@
         </div>
     </div>
     </form>
+    <form method="post" class="login">
+        <div><label>-----Usuario:</label>
+            <input name="user" type="text" >
+        </div>
+        <div><label>-----Contraseña:</label>
+            <input name="password" type="password">
+        </div>
+        <div>
+            <input name="login" type="submit" value="Ingresar">
+        </div>
+        <div><a href=registrar.php>Registrarse</a></div>
+        <a href=Invitado.php>Invitado</a>
+    </form>
+    
     <h6>
-    <footer>&copy; Sistema</footer>
-        <p>Sistema creado en la UV por estudiantes de la Licenciatura en Informática:</p>
-	<p>@Sirio</p>
-	<p>@Beatriz</p>
-	<p>@Julián</p>
-	<p>@Jesús</p>
+    
     </h6>
-</body>
-</html>
+    <?php $this->load->view('footer/footer_vista');?>
